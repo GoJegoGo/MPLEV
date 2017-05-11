f6268baf377cd462cc5c585e2200a187012a97a7
Troy Gabriel Ang Ko <troyangko05@gmail.com>
2017-05-11 09:45:32 +0800

    devise

--- enlistment_system/db/migrate/20170510083044_add_date_start_class_catalog.rb
+++
@@ -1,6 +0,0 @@
-class AddDateStartClassCatalog < ActiveRecord::Migration[5.0]
-  def change
-    add_column :class_catalog, :date_start, :date
-    add_column :class_catalog, :date_end, :date
-  end
-end
--- enlistment_system/db/migrate/20170510083446_remove_date_end_class_catalog.rb
+++
@@ -1,4 +0,0 @@
-class RemoveDateEndClassCatalog < ActiveRecord::Migration[5.0]
-  def change
-  end
-end
