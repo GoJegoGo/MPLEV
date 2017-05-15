6df7bb3c72e7e9ebac6a69db5abd5a61f590840a
Jego Reyes <joseph.gerard.reyes@obf.ateneo.edu>
2017-05-15 08:38:02 +0800

    final

--- enlistment_system/app/controllers/class_catalogs_controller.rb
+++ enlistment_system/app/controllers/class_catalogs_controller.rb
@@ -16,6 +16,7 @@ class ClassCatalogsController < ApplicationController
 
     def create
         @class_catalog = ClassCatalog.new(class_catalog_params)
+        @class_catalog.subject_list = "Algebra, Geometry, Trigonometry, Biology, Chemistry, Physics, Earth Science, Vocabulary, Grammar, Reading Comprehension, Filipino, Abstract and Logical Reasoning"
         if @class_catalog.save
             redirect_to class_catalog_path(@class_catalog.id)
         else
